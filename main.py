7dd4e365dface40ccd43bd2d7296cd13eac073f1
sagun-shakya <sagun.shakya@naamii.org.np>
2022-04-10 11:18:20 +0545

    sEPARATED ML MODELS AND DL MODELS IN CHOICES.

--- main.py
+++ main.py
@@ -26,6 +26,11 @@ rdm.seed(SEED)
 torch.manual_seed(SEED)
 torch.backends.cudnn.deterministic = True
 
+# Permissible models.
+ML_MODELS = ['svm', 'nb', 'random_forest', 'adaboost', 'logistic_regression']
+DL_MODELS = ['lstm', 'gru', 'cnn', 'mbert']
+MODEL_CHOICES = ML_MODELS + DL_MODELS
+
 
 def parse_arguments():
     '''
@@ -37,7 +42,6 @@ def parse_arguments():
         Arguments for the run.
 
     '''
-    MODEL_CHOICES = ['svm', 'nb', 'random_forest', 'adaboost', 'logistic_regression'] + ['lstm', 'gru', 'cnn', 'mbert']
     parser = argparse.ArgumentParser(description="Online IPV Detection argument parser.")
 
     parser.add_argument('-m', '--model', choices = MODEL_CHOICES,  default = 'lstm',
@@ -50,6 +54,26 @@ def parse_arguments():
     return args
 
 def train_dl_model(args: DotDict, logger: utils.log_object, device: str):
+    '''
+    Trains the Deep Learning models for different folds of data.
+    Store the validation results in a cache file named 
+    'Val_results_{args.model}_{args.train_type}_{args.learning_rate}_{utils.timestamp()}.csv'.
+
+    Parameters
+    ----------
+    args : DotDict
+        Arguments needed to train the model.
+        Comes jointly from the CLI and YAML file.
+    logger : utils.log_object
+        Logger object to store the logs.
+    device : str
+        CUDA or CPU.
+
+    Returns
+    -------
+    None.
+
+    '''
 
     cache_df = DataFrame(empty((5, 6)), 
                         columns = ['validation loss',
@@ -61,9 +85,10 @@ def train_dl_model(args: DotDict, logger: utils.log_object, device: str):
                         )
 
     logger.info(f'Training Started on {utils.current_timestamp()}.')\
-
+    
+        
     # Run training across different folds.
-    for k in [1]:
+    for k in [1,2,3]:
         data_loader = Dataloader(args, k, device)
         train_dl, val_dl = data_loader.load_data(args.batch_size)
 
@@ -75,6 +100,9 @@ def train_dl_model(args: DotDict, logger: utils.log_object, device: str):
 
         # Reset model weights to avoid weight leakage.
         utils.reset_weights(model)
+
+        # Count the number of traininable parameters.
+        logger.info(utils.count_parameters(model))
 
         # Set up trainer.
         trainer = Trainer(args, logger, data_loader, model, k)
