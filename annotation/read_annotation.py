50d6696ea224348b458d1167c9e5aca315f5fac8
sagun-shakya <sagun.shakya@naamii.org.np>
2022-04-13 08:14:21 +0545

    Added parse file and clean text.

--- annotation/read_annotation.py
+++ annotation/read_annotation.py
@@ -21,9 +21,20 @@ annot2 = r'annotation\data\shr'
 
 example = 'batch_ipv_600-610 exported.tsv'
 
-def run():
-    filename = join(annot2, example)
+def parse_file(filename: str) -> pd.DataFrame:
+    """
+    Parses a WebAnno TSV v3 file to a DataFrame.
+    Preprocessing involved:
+        - Converts the aspect category annotations to BIO format.
+        - Removes the sentence numbers associated with the annotations in square brackets. e.g. 1[8] --> 1.
+        - Removes escape characters like "\".
 
+    Args:
+        filename (str): Path to the TSV file.
+
+    Returns:
+        pd.DataFrame: DataFrame with columns -- ['s_no, str_id, token, ac, ap, conf, ipv']
+    """
     # Getting the exported TSV file.
     text = get_file(filename)
     text = [tt for tt in text if tt != '\n']
@@ -40,6 +51,30 @@ def run():
         else:
             storage.append(line.split())
     
-    print(storage[:10])
-if __name__ == "__main__":
-    run()
+    # Define DataFrame.
+    df = pd.DataFrame(storage, columns = 's_no, str_id, token, ac, ap, conf, ipv'.split(", "))
+
+    # Convert to BIO format.
+    df['ac'] = convert_to_bio(df['ac'].to_list())
+
+    # Replace "\\_" with "_" in ac.
+    df['ac'] = df['ac'].apply(lambda x: x.replace('\\_', '_'))
+
+    # Fixing aspect polarity.
+    df['ap'] = df['ap'].apply(lambda x: x[0] if x != '_' else x)
+
+    return df
+
+def merge_annotations(filename_list: list) -> pd.DataFrame:
+    """
+    Merges the dataframes generated from parse_file function.
+
+    Args:
+        filename_list (list): List of filepaths to be parsed.
+
+    Returns:
+        pd.DataFrame: DataFrame contaning all the annotations from the files provided.
+    """    
+    storage = [parse_file(file) for file in filename_list]
+    res = pd.concat(storage, axis = 0).reset_index(drop = True)
+    return res
