8feca53f2171139ed4e24b9460405fbae3192b9c
sagun-shakya <sagun.shakya@naamii.org.np>
2022-04-25 12:40:07 +0545

    Convert annotations to csv.

--- exp.py
+++ exp.py
@@ -4,27 +4,36 @@ from os.path import join
 from annotation.read_annotation import *
 from annotation.agreement import PairwiseAgreement
 
-def run():
+def get_processed_data():
     # Done by Sharmila.
     shr_root = r'D:\ML_projects\IPV-Project\annotation\data\ipv\Sharmila'
-    krn_verified = r'D:\ML_projects\IPV-Project\annotation\data\CROSS\Kiran Cross checked files'
+    #krn_verified = r'D:\ML_projects\IPV-Project\annotation\data\CROSS\Kiran Cross checked files'
     
     # Done by Kiran.
     krn_root = r'D:\ML_projects\IPV-Project\annotation\data\ipv\kiran'
-    shr_verified = r'D:\ML_projects\IPV-Project\annotation\data\CROSS\Sharmila Cross checked files'
+    #shr_verified = r'D:\ML_projects\IPV-Project\annotation\data\CROSS\Sharmila Cross checked files'
 
     shr_files = [file[:-4] for file in os.listdir(shr_root)]
     krn_files = [file[:-4] for file in os.listdir(krn_root)]
 
+    # For agreement calculation.
     target = np.intersect1d(shr_files, krn_files)
     print(f'Number of files to inspect : {len(target)}\n')
 
     shr_target_filenames = [os.path.join(shr_root, file + '.tsv') for file in target]
     krn_target_filenames = [os.path.join(krn_root, file + '.tsv') for file in target]
 
-    df_shr = merge_annotations(shr_target_filenames)
-    df_krn = merge_annotations(krn_target_filenames)
+    # For getting dataframes.
+    shr_filenames = [os.path.join(shr_root, file) for file in os.listdir(shr_root)]
+    krn_filenames = [os.path.join(krn_root, file) for file in os.listdir(krn_root)]
+
+    df_shr = merge_annotations(shr_filenames)
+    df_krn = merge_annotations(krn_filenames)
+
+    return df_shr, df_krn
     
+
+def get_agreement(df_shr, df_krn):
     shr_agreement = PairwiseAgreement(df_shr, df_krn, 'token', 1.0)
     agreement_dict = shr_agreement.calculate_agreement()
     df = pd.DataFrame(agreement_dict, index=[0]).T.round(3)
@@ -32,4 +41,6 @@ def run():
     print(agreement_dict)
 
 if __name__ == "__main__":
-    run()
+    df_shr, df_krn = get_processed_data()
+    df_shr.to_csv(os.path.join(r"annotation\data\aspect_extraction_sample", 'shr_asp.csv'), index = None, encoding='utf-8')
+    df_krn.to_csv(os.path.join(r"annotation\data\aspect_extraction_sample", 'krn_asp.csv'), index = None, encoding='utf-8')
