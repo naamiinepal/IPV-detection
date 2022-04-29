9f7a4797152d4afb43a795bb5f0d9036f7bac47c
sagun-shakya <sagun.shakya@naamii.org.np>
2022-04-29 05:48:22 +0545

    K fold spilt for aspect extraction.

--- utilities/kfold_split_asp_extraction.py
+++ utilities/kfold_split_asp_extraction.py
@@ -19,51 +19,9 @@ from sklearn.model_selection import StratifiedKFold, KFold
 
 # Local Modules.
 from utilities import utils
-from read_configuration import DotDict
 
 
-def argument_parser():
-    parser = argparse.ArgumentParser(description = "Argument Parser for Creating K fold splits fr aspect term extraction.")
-    parser.add_argument('-i', '--input_dir', default = r'data/aspect_extraction', type = str, metavar = 'PATH',
-                        help = 'Path to raw data directory.')
-    parser.add_argument('-o', '--save_dir', default = r'data/aspect_extraction/kfold', type = str, metavar='PATH',
-                        help = 'Path to the data directory to store K fold splits.')
-    parser.add_argument('--log_dir', default = r'logs/splits', type = str, metavar = 'PATH',
-                        help = 'Directory to save logs.')
-    parser.add_argument('-k', '--kfolds', default=5, type=int, 
-                        help = 'Number of folds to generate.')                    
-    parser.add_argument('-r', '--random_seed', default=1234, type=int, 
-                        help = 'Random seed.')
-    parser.add_argument('-v', '--verbose', action = 'store_true', 
-                        help = 'Whether to display verbose.')
-    args = parser.parse_args()
-    return args
-
-def log_object(args): 
-    '''
-    Generates a logger object.
-
-    Parameters
-    ----------
-    args : DotDict object.
-        Arguments for the project.
-
-    Returns
-    -------
-    logger : logger object on which log information can be written.
-    '''      
-    
-    # If the log directory does not exist, we'll create it.
-    os.makedirs(args.log_dir, exist_ok = True)
-
-    name_ = f'asp_extraction_{args.kfolds}_fold_split.log'
-    log_file = os.path.join(args.log_dir, name_)
-
-    # Intialize Logger.
-    logger = utils.get_logger(log_file)
-    return logger
-
-def main(args, logger):
+def kfold_split_aspect(args, logger):
     #%% Read raw data.
 
     root = r'D:\ML_projects\IPV-Project\data\aspect_extraction'
@@ -103,6 +61,29 @@ def main(args, logger):
     print(f'Shape after: {shape_after}.')
     print(f'Number of sentences with only "O" tags removed : {shape_before[0] - shape_after[0]}.\n')
 
+    # K - fold splits.
+    kf = KFold(n_splits = args.kfolds, random_state = args.random_seed, shuffle = True)
+
+    for k, (train_id, val_id) in enumerate(kf.split(data['tokens'], data['labels']), 1):
+        train = data.loc[train_id, :]
+        val = data.loc[val_id, :]
+
+        # Pre-processing.
+
+        # Info.
+        logger.info(f'\nLength of training set : {len(train)}')
+        logger.info(f'Length of validation set : {len(val)}\n')
+
+        # Save files to CONLL2003 format.
+        save_filepath = join(args.save_dir, str(k))
+        os.makedirs(save_filepath, exist_ok=True)
+        
+        logger.info(f'Saving files at : {save_filepath}...\n')
+
+        utils.write_to_CONLL(train, join(save_filepath, 'train.txt'))
+        utils.write_to_CONLL(val, join(save_filepath, 'val.txt'))
+        
+        logger.info(f"Success! Save date : {utils.current_timestamp()}\n")
 
 
 
