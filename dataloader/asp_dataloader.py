8e314fcd84eb341a2b8500b516387a70eb38135c
sagun-shakya <sagun.shakya@naamii.org.np>
2022-05-01 15:46:15 +0545

    Asp extraction corpus for tansofrmer.

--- dataloader/asp_dataloader.py
+++ dataloader/asp_dataloader.py
@@ -1,37 +1,47 @@
+import os
+from typing import Tuple
+from numpy import ones
+from torch import as_tensor, index_put_
 from torchtext.legacy.data import Field, BucketIterator
 from torchtext.legacy.datasets import SequenceTaggingDataset
+from torch.utils.data import Dataset, DataLoader
+from transformers import BertTokenizerFast, BertConfig, BertForTokenClassification
 from transformers import BertTokenizer
+
+from utilities.utils import flatten, read_CoNLL
 
 class AspectExtractionCorpus:
 
-	def __init__(self, args, input_directory, device):
+	def __init__(self, model, input_directory, device, model_name):
 		
 		self.input_directory = input_directory
 		self.device = device
+		self.model = model
 
-		if self.args.model == 'bert' or self.args.model == 'muril':
-            self.bert_tokenizer = BertTokenizer.from_pretrained(self.args.bert.model_name) 
-            self.PAD_INDEX = self.bert_tokenizer.pad_token
-            self.CLS_INDEX = self.bert_tokenizer.cls_token
-            self.UNK_INDEX = self.bert_tokenizer.unk_token
+		# List all the fields.
+		if self.model in ["muril", "mbert"]:
+			self.bert_tokenizer = BertTokenizer.from_pretrained(model_name)
+			self.PAD_INDEX = self.bert_tokenizer.pad_token
+			self.CLS_INDEX = self.bert_tokenizer.cls_token
+			self.UNK_INDEX = self.bert_tokenizer.unk_token
 
-			self.word_field = Field(tokenize = self.bert_tokenizer.tokenize,
-									batch_first = True,
+			self.word_field = Field(batch_first = True,
+									sequential = False,
 									pad_token = self.PAD_INDEX,
 									init_token = self.CLS_INDEX,
 									unk_token = self.UNK_INDEX)
-		# List all the fields.
+		
 		else:
-			self.word_field = Field(batch_first=True)
+			self.word_field = Field(batch_first = True)
 
-		self.tag_field = Field(unk_token=None, batch_first=True)
+		self.tag_field = Field(unk_token = None, batch_first = True)
 		self.FIELDS = (("word", self.word_field), ("tag", self.tag_field))
 
 		# Create train and validation dataset using built-in parser from torchtext.
 		self.train_ds, self.val_ds = SequenceTaggingDataset.splits(path = input_directory, 
-																train = 'train.txt', 
-																validation = 'val.txt', 
-																fields = self.FIELDS)
+																	train = 'train.txt', 
+																	validation = 'val.txt', 
+																	fields = self.FIELDS)
 
 		# Convert fields to vocabulary list.
 		self.word_field.build_vocab(self.train_ds.word)   # ADD VECTORS HERE.
@@ -81,3 +91,79 @@ class AspectExtractionCorpus:
 															device = self.device)
 
 		return train_dl, val_dl                                                      
+
+
+class AspectDataset(Dataset):
+  def __init__(self, input_directory, tokenizer, max_len, labels_to_ids = None, is_train = True):
+        self.input_directory = input_directory
+        self.tokenizer = tokenizer
+        self.max_len = max_len
+
+        self.sentences, self.labels = read_CoNLL(self.input_directory)
+        self.tags_list = set(flatten(self.labels))
+
+        if is_train:
+              self.labels_to_ids = {k: v for v, k in enumerate(self.tags_list)}
+              self.ids_to_labels = {v: k for v, k in enumerate(self.tags_list)}
+        else:
+              self.labels_to_ids = labels_to_ids
+              self.ids_to_labels = {v : k for k, v in self.labels_to_ids.items()}
+
+
+  def __getitem__(self, index):
+
+        # step 1: get the sentence and word labels 
+        sentence = self.sentences[index]  
+        word_labels = self.labels[index] 
+
+        # step 2: use tokenizer to encode sentence (includes padding/truncation up to max length)
+        # BertTokenizerFast provides a handy "return_offsets_mapping" functionality for individual tokens
+        encoding = self.tokenizer(sentence,
+                                  is_split_into_words=True, 
+                                  return_offsets_mapping=True, 
+                                  padding='max_length', 
+                                  truncation=True, 
+                                  max_length=self.max_len)
+        
+        # step 3: create token labels only for first word pieces of each tokenized word
+        labels = [self.labels_to_ids[label] for label in word_labels] 
+        
+        # code based on https://huggingface.co/transformers/custom_datasets.html#tok-ner
+        # create an empty array of -100 of length max_length
+        encoded_labels = ones(len(encoding["offset_mapping"]), dtype=int) * -100
+        
+        # set only labels whose first offset position is 0 and the second is not 0
+        i = 0
+        for idx, mapping in enumerate(encoding["offset_mapping"]):
+          if mapping[0] == 0 and mapping[1] != 0:
+            # overwrite label
+            encoded_labels[idx] = labels[i]
+            i += 1
+
+        # step 4: turn everything into PyTorch tensors
+        item = {key: as_tensor(val) for key, val in encoding.items()}
+        item['labels'] = as_tensor(encoded_labels)
+
+        # Get lengths.
+        item['seq_length'] = sum(encoding['attention_mask'])
+        
+        return item
+
+  def __len__(self):
+        return len(self.sentences)
+
+
+class AspectExtractionCorpus_Transformer:
+	def __init__(self, input_directory, tokenizer, max_len) -> None:
+		self.input_directory = input_directory
+		self.max_len = max_len
+		self.tokenizer = tokenizer
+
+		self.train_ds = AspectDataset(os.path.join(self.input_directory, "train.txt"), tokenizer, max_len)
+		self.labels_to_ids = self.train_ds.labels_to_ids
+		self.val_ds = AspectDataset(os.path.join(self.input_directory, "val.txt"), self.tokenizer, self.max_len, self.labels_to_ids, is_train=False)
+
+	def load_data(self, batch_sizes: Tuple[int], shuffle: bool = True):
+		train_dl = DataLoader(self.train_ds, batch_size = batch_sizes[0], shuffle = shuffle)
+		val_dl = DataLoader(self.train_ds, batch_size = batch_sizes[1], shuffle = shuffle)
+		return train_dl, val_dl
