40118228f14735380c9d0023e309737fbdf0544f
sagun-shakya <sagun.shakya@naamii.org.np>
2022-05-08 17:04:44 +0545

    Remove already annotated tweets.

--- annotation/create_batches.py
+++ annotation/create_batches.py
@@ -14,8 +14,9 @@ import numpy as np
 import os
 from os.path import join, exists
 from warnings import filterwarnings
-filterwarnings('ignore')
 
+from tqdm import tqdm
+filterwarnings('ignore')
 
 # For Twitter Scraped data.
 def create_batches_twitter(root_dir, filename, save_dir):
@@ -27,9 +28,27 @@ def create_batches_twitter(root_dir, filename, save_dir):
     elif filename.endswith('.csv'):
         df = pd.read_csv(filepath, encoding = 'utf-8', skip_blank_lines = True)
     
-    text = df['text']
+    # Make sure the tweets are not already included in the master data file "overall.xlsx".
+    df_overall = pd.read_excel(r'D:\ML_projects\IPV-Project\annotation\data\overall.xlsx')
+
+    # Accumulate already annotated tweets links.
+    df_overall_link = df_overall[df_overall['source'] == 'twitter']['links']
+    df_link = df['link']
+    common_tweets = np.intersect1d(df_overall_link, df_link)
+
+    # Verbose.
+    print(f"Found {len(common_tweets)} tweets which have already been annotated.")
+    print('Excluding these tweets for creating batches...')
+    print(common_tweets)
+    print()
+
+    # Delete rows.
+    df.drop(df[df['link'].isin(common_tweets)].index, inplace = True)
+    df.reset_index(drop = True, inplace = True)
 
     # Clean Text.
+    text = df['text']
+    
     ## Remove punctuations.
     other_punctuations = '।‘’' + '!"#$%&\'()*+,./:;<=>?@[\\]^_`{|}~' + chr(8211)
     to_remove = punctuation + other_punctuations + chr(8226)     
@@ -50,7 +69,7 @@ def create_batches_twitter(root_dir, filename, save_dir):
     os.makedirs(save_dir_shr, exist_ok = True)
     os.makedirs(save_dir_krn, exist_ok = True)
 
-    for k, ii in enumerate(range(0, len(text), 10), 1):
+    for k, ii in tqdm(enumerate(range(0, len(text), 10), 1)):
         start = ii
         end = ii + 10
         batch = text.loc[start:end].str.strip()
@@ -66,4 +85,4 @@ def create_batches_twitter(root_dir, filename, save_dir):
 if __name__ == "__main__":
     create_batches_twitter(root_dir=r'D:\ML_projects\IPV-Scraper\results\second_lot',
                             filename='df_merged_second_lot_search_terms_cleaned_05-May-2021_nep.xlsx',
-                            save_dir=r'D:\ML_projects\IPV-Scraper\results\second_lot\sample_folder')
+                            save_dir=r'D:\ML_projects\IPV-Scraper\results\second_lot\sample_folder1')
