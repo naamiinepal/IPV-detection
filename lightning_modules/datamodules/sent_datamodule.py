cc6494f6ab0539c99e4c1b81f23a9e80c4a525e5
Rabin Adhikari <rabin.adk1@gmail.com>
2022-12-28 12:07:43 +0545

    Load current folds in the sent datamodule

--- lightning_modules/datamodules/sent_datamodule.py
+++ lightning_modules/datamodules/sent_datamodule.py
@@ -17,6 +17,7 @@ class SentDataModule(BaseDataModule):
         dataset_path: str,
         val_ratio: float = 0.1,
         use_cache: bool = True,
+        current_fold: Optional[int] = None,
         **kwargs,
     ):
         super().__init__(**kwargs)
@@ -31,31 +32,39 @@ class SentDataModule(BaseDataModule):
     def setup(self, stage: Optional[str] = None):
         dataset_path = self.hparams.dataset_path
 
+        tokenized_root = f"{dataset_path}_sent_cache"
+
+        current_fold = self.hparams.current_fold
+        if current_fold is not None:
+            tokenized_root = f"{tokenized_root}_fold_{current_fold}"
+
         tokenized_path = os.path.join(
-            f"{dataset_path}_sent_cache",
+            tokenized_root,
             self.hparams.model_name_or_path.replace("/", "_"),
         )
 
+        kfold_dir = os.path.join(dataset_path, "folds", str(current_fold))
+
         # Assign train/val datasets for use in dataloaders
         if stage is None or stage == "fit" or stage == "validate":
-
             if self.hparams.use_cache and os.path.isdir(tokenized_path):
                 dataset_full = load_from_disk(tokenized_path)
             else:
-                dataset_full = (
-                    load_dataset(
+                if current_fold is None:
+                    dataset_full = load_dataset(
                         "csv",
                         data_files=os.path.join(dataset_path, "combined.csv"),
                         split="train",
-                    )
-                    .train_test_split(test_size=self.hparams.val_ratio)
-                    .map(
-                        self.tokenize,
-                        batched=True,
-                        batch_size=1024,
-                        remove_columns="text",
-                        num_proc=self.num_workers,
-                    )
+                    ).train_test_split(test_size=self.hparams.val_ratio)
+                else:
+                    dataset_full = load_dataset("csv", data_dir=kfold_dir)
+
+                dataset_full = dataset_full.map(
+                    self.tokenize,
+                    batched=True,
+                    batch_size=1024,
+                    remove_columns="text",
+                    num_proc=self.num_workers,
                 )
 
                 dataset_full.save_to_disk(tokenized_path)
@@ -72,8 +81,10 @@ class SentDataModule(BaseDataModule):
                 # Needed for writing to the predictions file
                 self.dataset_full = load_dataset(
                     "csv",
-                    data_files=os.path.join(dataset_path, "combined.csv"),
-                    split="train",
+                    data_files=os.path.join(dataset_path, "combined.csv")
+                    if current_fold is None
+                    else os.path.join(kfold_dir, "test.csv"),
+                    split="train",  # Default is train for every loading
                 ).map(
                     self.tokenize,
                     batched=True,
